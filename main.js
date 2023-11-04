0c535005f0ad61e1f29c3eacb1c06ab38b979ca6
arvind <achurox1121@gmail.com>
2023-11-04 20:06:59 +0530

    add additional text field for result, add numbers/operators to buttons, add feature to create expression w/ buttons

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//DOM ELEMENTS
+const expression = document.querySelector('#expression');
+const result = document.querySelector('#result');
+const button = document.querySelectorAll('div#bottom>*');
+const clear = document.querySelector('#C');
+
 let add =(a,b)=> a+b;
 let subtract = (a,b)=>a-b;
 let multiply = (a,b)=>a*b;
@@ -7,5 +13,19 @@ let first;
 let second;
 let operator;
 
+console.log(button);
+expression.value="";
+let str;
+for(var inp of button){
+    inp.addEventListener("click", write);
+}
+
+function write(){
+    expression.value = expression.value.concat(this.innerHTML);
+}
+
+clear.addEventListener("click", function(){
+    expression.value = "";
+})
 function operate(first,second,operator){};
 
