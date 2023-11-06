c3970677784874a442c643706234cec5554e28fe
arvind <achurox1121@gmail.com>
2023-11-07 00:20:18 +0530

    add brackets functionality, update more rules to operators and numbers, make expression creation strict

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const expression = document.querySelector('#expression');
 const result = document.querySelector('#result');
 const num = document.querySelectorAll('div#bottom>.num');
-const operator = document.querySelectorAll('div#bottom>.op');
+const operator = document.querySelectorAll('.op');
 const equal = document.querySelector('.eq')
 const clear = document.querySelector('#C');
 const back = document.querySelector('#back');
@@ -19,33 +19,64 @@ clear.addEventListener("click", function(){
 back.addEventListener("click", function(){
     expression.value = expression.value.slice(0,-1);
 })
+
+//loops to add click listeners for buttons
 for(var inp of num){
     inp.addEventListener("click", write);
 }
 for(var inp of operator){
     inp.addEventListener("click", writeop);
 }
+
 //function on num click
 function write(){
+    if(expression.value.slice(-1)==')'){
+        return;
+    }
     expression.value = expression.value.concat(this.innerHTML);
 }
 //function on op click
 oparray = ["+","-","÷","x"];
-pattern = /\.+/;
+dotpattern= /\.+/;
+flag=0;
 function writeop(){
+    last= expression.value.slice(-1);
+    current = this.innerHTML
     //string should not be empty
     if (expression.value==""){
         return;
     }
-    //last element should not be operator
-    if (oparray.includes(expression.value.slice(-1))){
+    //last element should not be operator or (
+    if ((oparray.includes(last) && current!='(' && current!=')' )|| last=="("){
         return;
+    }
+    if(!oparray.includes(last) && current=='('){
+        current='x(';
+        flag+=1;
     }
     //no more than 1 dot
-    if (pattern.test(expression.value) && this.innerHTML=='.'){
+    if ((dotpattern.test(expression.value) && current=='.' )||(current=='.' && last==')')){
         return;
     }
-    expression.value = expression.value.concat(this.innerHTML);
+    if (current == '('){
+        flag+=1;
+    }
+    if(current == ')'){
+        if(flag==0 || oparray.includes(last)){
+            return;
+        }
+        flag-=1;
+    }
+    expression.value = expression.value.concat(current);
 }
-function operate(first,second,operator){};
+
+//stack
+stack = [];
+
+//function on equal click
+equal.addEventListener("click",operate);
+function operate(){
+    
+}
+
 
