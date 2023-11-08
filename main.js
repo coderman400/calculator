1fe142dc849ceffde0beca69a616575c2d3fd4fd
arvind <achurox1121@gmail.com>
2023-11-08 21:53:03 +0530

    Add stacks for evaluating infix, create code to read and operate on function, some errors remain

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let add =(a,b)=> a+b;
 let subtract = (a,b)=>a-b;
 let multiply = (a,b)=>a*b;
 let divide = (a,b)=>a/b;
-
+let resultnum;
 //CLEAR 
 clear.addEventListener("click", function(){
     expression.value = "";
@@ -70,13 +70,109 @@ function writeop(){
     expression.value = expression.value.concat(current);
 }
 
-//stack
-stack = [];
-
+//check if number
+function isNumeric(num){
+    return !isNaN(num)
+  }
+function precedence(operator){
+    if(['÷','x'].includes(operator)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+function calculate(a,b,operator){
+    console.log("a,op,b -> ",a,operator,b);
+    switch(operator){
+        case '+': 
+            resultnum = a+b;
+            break;
+        case '-':
+            resultnum = a-b;
+            break;
+        case 'x':
+            resultnum = a*b;
+            break;
+        case '÷':
+            resultnum = a/b;
+            break;
+    }
+    return resultnum;
+}
 //function on equal click
 equal.addEventListener("click",operate);
 function operate(){
-    
+    ostack=[]
+    nstack=[]
+    exp = expression.value;
+    if (flag!=0){
+        result.value =("BRACKETS INVALID");
+        return;
+    }
+    length = exp.length;
+    number="";
+    for(let i=0; i<length; i++){
+        if(exp[i]===undefined){
+            continue;
+        }
+        //if number, push to nstack
+        while(isNumeric(exp[i]) || exp[i]=='.'){
+            number = number.concat(exp[i]);
+            i+=1;
+        }
+        nstack.push(+number);
+        number=""
+        //if operand, check if empty (push if yes)
+        if(!isNumeric(exp[i])){
+            if(exp[i] === undefined){
+                continue;
+            }
+            if(ostack.length==0){
+                ostack.push(exp[i]);
+                continue;
+            }else{
+                //if ( push, if ) pop until (
+                if(exp[i]=='('){
+                    ostack.push(exp[i]);
+                    continue;
+                }else if(exp[i]==')'){
+                    while(ostack[ostack.length-1]!='('){
+                        b = nstack.pop();
+                        a = nstack.pop();
+                        opr = ostack.pop();
+                        nstack.push(calculate(a,b,opr));
+                    }
+                    ostack.pop();
+                    continue;
+                }
+                //else check precedence with top
+                //if currents precedence >= top then push
+                if(precedence(exp[i])>=precedence(ostack[ostack.length-1])){
+                    ostack.push(exp[i]);
+                }else{
+                //else pop until above is satisfied
+                    while(precedence(exp[i])<precedence(ostack[ostack.length-1])){
+                        b = nstack.pop();
+                        a = nstack.pop();
+                        opr = ostack.pop();
+                        nstack.push(calculate(a,b,opr));
+                    }
+                    ostack.push(exp[i]);
+                }
+            }
+        }
+        console.log("ostack = ",ostack);
+        console.log("nstack = ",nstack);
+    }
+    console.log("before loop ostack is ", ostack);
+    while(ostack.length!=0){
+        console.log("popping");
+        b = nstack.pop();
+        a = nstack.pop();
+        opr = ostack.pop();
+        nstack.push(calculate(a,b,opr));
+    }
+    console.log(resultnum);
 }
 
 
